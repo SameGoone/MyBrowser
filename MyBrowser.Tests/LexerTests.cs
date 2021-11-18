c12dabad9e6410ecd3205ce717f2cf97aa2238da
SameGoone <ovechenko.n.r@gmail.com>
2021-11-18 17:10:33 +0300

    Избавился от дублирования кода. Добавлены комментарии

--- MyBrowser.Tests/LexerTests.cs
+++ MyBrowser.Tests/LexerTests.cs
@@ -6,108 +6,97 @@ namespace MyBrowser.Tests
     [TestClass]
     public class LexerTests
     {
+        /// <summary>
+        /// Проверяет правильность разбиения на токены фрагментов HTML-кода
+        /// </summary>
         [TestMethod]
         public void TestLexingHTML()
         {
             string code = "<body atr1=\"5\">Hello World!</body>";
-            Lexer lexer = new Lexer(code);
-            List<object> tokens = new List<object>();
             object[] expectedTokens = new object[] { '<', "body", ' ', "atr1", '=', '"', 5, '"', '>', "Hello", ' ', "World!", '<', '/', "body", '>' };
 
-            object token;
-            while (lexer.TryGetToken(out token))
-            {
-                tokens.Add(token);
-            }
-
-            Assert.AreEqual(expectedTokens.Length, tokens.Count);
-
-            for (int i = 0; i < tokens.Count; i++)
-            {
-                Assert.AreEqual(expectedTokens[i], tokens[i]);
-            }
+            TestLex(code, expectedTokens);
         }
 
+        /// <summary>
+        /// Проверяет правильность разбиения на токены фрагментов CSS-кода
+        /// </summary>
         [TestMethod]
         public void TestLexingCSS()
         {
             string code = "body{font-family:Arial,Verdana,sans-serif; font-size:11pt;}";
-            Lexer lexer = new Lexer(code);
-            List<object> tokens = new List<object>();
             object[] expectedTokens = new object[] { "body", '{', "font-family", ':', "Arial", ',', "Verdana", ',' , "sans-serif", ';', ' ', "font-size", ':', 11, "pt", ';', '}' };
 
-            object token;
-            while (lexer.TryGetToken(out token))
-            {
-                tokens.Add(token);
-            }
-
-            Assert.AreEqual(expectedTokens.Length, tokens.Count);
-
-            for (int i = 0; i < tokens.Count; i++)
-            {
-                Assert.AreEqual(expectedTokens[i], tokens[i]);
-            }
+            TestLex(code, expectedTokens);
         }
 
+        /// <summary>
+        /// Проверяет правильность значений и последовательности токенов введённой строки
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="expectedTokens"></param>
+        [TestMethod]
+        private void TestLex(string code, object[] expectedTokens)
+        {
+            var tokens = Analyze(code);
+            CollectionAssert.AreEqual(expectedTokens, tokens);
+        }
+
+        /// <summary>
+        /// Проверяет правильность выделения целых чисел из кода
+        /// </summary>
         [TestMethod]
         public void TestLexingIntNumbers()
         {
             string stringWithPositiveInt = "margin: 10px;";
             string stringWithNegativeInt = "margin: -10px;";
-            Lexer lexer;
-            List<object> tokens;
-            object token;
 
-            //Тест положительных
-            lexer = new Lexer(stringWithPositiveInt);
-            tokens = new List<object>();
-            while (lexer.TryGetToken(out token))
-            {
-                tokens.Add(token);
-            }
-            Assert.IsInstanceOfType(tokens[3], typeof(int));
-            Assert.AreEqual(10, tokens[3]);
-
-            //Тест отрицательных
-            lexer = new Lexer(stringWithNegativeInt);
-            tokens = new List<object>();
-            while (lexer.TryGetToken(out token))
-            {
-                tokens.Add(token);
-            }
-            Assert.IsInstanceOfType(tokens[3], typeof(int));
-            Assert.AreEqual(-10, tokens[3]);
+            TestNums(stringWithPositiveInt, 3, 10);
+            TestNums(stringWithNegativeInt, 3, -10);
         }
 
+        /// <summary>
+        /// Проверяет правильность выделения чисел с плавающей точкой из кода
+        /// </summary>
         [TestMethod]
         public void TestLexingDoubleNumbers()
         {
             string stringWithPositiveDouble = "margin: 2.5em;";
             string stringWithNegativeDouble = "margin: -2.5em;";
-            Lexer lexer;
-            List<object> tokens;
+
+            TestNums(stringWithPositiveDouble, 3, 2.5);
+            TestNums(stringWithNegativeDouble, 3, -2.5);
+        }
+
+        /// <summary>
+        /// Проверяет правильность выделения чисел из кода
+        /// </summary>
+        /// <param name="code">Строка для лексического разбора</param>
+        /// <param name="index">Ожидаемый индекс, по которому располагается число</param>
+        /// <param name="value">Ожидаемое значение числа</param>
+        [TestMethod]
+        private void TestNums(string code, int index, object value)
+        {
+            var tokens = Analyze(code);
+            Assert.AreEqual(value, tokens[index]);
+        }
+
+        /// <summary>
+        /// Разбивает строку на токены
+        /// </summary>
+        /// <param name="code">Строка для разбиения</param>
+        /// <returns>Коллекция полученных из строки токенов</returns>
+        private List<object> Analyze(string code)
+        {
+            Lexer lexer = new Lexer(code);
+            List<object> tokens = new List<object>();
             object token;
-
-            //Тест положительных
-            lexer = new Lexer(stringWithPositiveDouble);
-            tokens = new List<object>();
             while (lexer.TryGetToken(out token))
             {
                 tokens.Add(token);
             }
-            Assert.IsInstanceOfType(tokens[3], typeof(double));
-            Assert.AreEqual(2.5, (double)tokens[3], 0.001);
 
-            //Тест отрицательных
-            lexer = new Lexer(stringWithNegativeDouble);
-            tokens = new List<object>();
-            while (lexer.TryGetToken(out token))
-            {
-                tokens.Add(token);
-            }
-            Assert.IsInstanceOfType(tokens[3], typeof(double));
-            Assert.AreEqual(-2.5, (double)tokens[3], 0.001);
+            return tokens;
         }
     }
 }
